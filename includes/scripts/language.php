6706cc14e187136c8ae719015155dbc89a22afe2
Damian <damgiankakis@gmail.com>
2017-06-10 19:43:39 +0300

    Adds live change of language for seo

--- includes/scripts/language.php
+++ includes/scripts/language.php
@@ -7,13 +7,19 @@ class CookieLanguage {
     public $value = 'el';
 
     function __construct() {
-        if(!isset($_COOKIE['pd_lang']) || empty($_COOKIE['pd_lang']) || !in_array($_COOKIE['pd_lang'], $this->languages)) {
-            $this->value = 'el';
-            setcookie('pd_lang', $this->value, time() + 86400 * 5);
+        if(!isset($_GET['pd_lang']) || empty($_GET['pd_lang']) || !in_array($_GET['pd_lang'], $this->languages)) {
+            if(!isset($_COOKIE['pd_lang']) || empty($_COOKIE['pd_lang']) || !in_array($_COOKIE['pd_lang'], $this->languages)) {
+                $this->value = 'el';
+                setcookie('pd_lang', $this->value, time() + 86400 * 5);
+            } else {
+                $this->value = $_COOKIE['pd_lang'];
+            }
+            define('COOKIE', $this->value);
         } else {
-            $this->value = $_COOKIE['pd_lang'];
+            $this->value = $_GET['pd_lang'];
+            setcookie('pd_lang', $this->value, time() + 86400 * 5);
+            define('COOKIE', $this->value);
         }
-        define('COOKIE', $this->value);
     }
 }
 ?>
