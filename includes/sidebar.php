3caf7d47d5e109a50027c8ae3c26213effa0ab8c
Damian <damgiankakis@gmail.com>
2017-06-10 12:32:13 +0300

    Initial commit

---
+++ includes/sidebar.php
@@ -0,0 +1,21 @@
+			<aside id="sibebar" class="scol30 mcol30 lcol30">
+				<div class="menu" langtype="el">
+					<ul>
+						<li><span class="icon-align-justify"></span></li>
+						<li><a href="index.php" title="Home">Αρχική</a></li>
+						<li><a href="services.php" title="Services">Υπηρεσίες</a></li>
+						<li><a href="articles.php" title="Articles">Άρθρα</a></li>
+						<li><a href="contact.php" title="Contact">Επικοινωνία</a></li>
+					</ul>
+				</div>
+				<div class="menu" langtype="en">
+					<ul>
+						<li><i class="fa fa-bars"></i></li>
+						<li><a href="index.php" title="Home">Home</a></li>
+						<li><a href="services.php" title="Services">Services</a></li>
+						<li><a href="articles.php" title="Articles">Articles</a></li>
+						<li><a href="contact.php" title="Contact">Contact</a></li>
+					</ul>
+				</div>
+				<a id="twitter-timeline" class="twitter-timeline" data-width="370" data-height="500" data-lang="en" data-chrome="nofooter" tweet-limit="10" href="https://twitter.com/podokomia">Tweets by podokomia</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+			</aside>
---
+++ index.php
@@ -0,0 +1,17 @@
+﻿<?php require_once("includes/head.php"); ?>
+<body id="el" pg_title_el="Αρχική Σελίδα" pg_title_en="Homepage">
+    <?php require_once("includes/header.php"); ?>
+		<main id="main">
+<?php require_once("includes/sidebar.php") ?>
+            <div class="scol70 mcol70 lcol70">
+                <h2 class="welcome" langtype="el">ΚΑΛΩΣ ΗΡΘΑΤΕ</h2>
+                <p class="desc" langtype="el">
+                    Ο Βασίλης Ζαφειράκης είναι κτηνίατρος παραγωγικών ζώων, απόφοιτος του ΑΠΘ το  1996. Έχει πιστοποίηση απο το Hoof Τrimming Seminar PTC  Oenkerk Netherlands (2001) στην ποδοκομία αγελάδων. Ασχολείται κυρίως με τον θεραπευτικό εξονυχισμό των αγελάδων και την διαχείριση του προβλήματος των χωλοτήτων.
+                    Δραστηριοποιείται στη Θεσσαλονίκη και στις γύρω περιοχές.
+                </p>
+                <h2 class="welcome" langtype="en">Welcome</h2>
+                <p class="desc" langtype="en">Vasilis Zafeirakis is a dairy vet who graduated from the Aristotle University of Thessaloniki in 1996. He has attended the Hoof Τrimming Seminar PTC Oenkerk in Netherlands (2001). He is particularly interested in bovine lameness, hoof trimming, surgery, internal medicine and nutrition.</p>
+                <img alt="main-page-cow-image" src="images/home.jpg" id="home-img"/>
+            </div>
+		</main>
+<?php require_once("includes/footer.php"); ?>
