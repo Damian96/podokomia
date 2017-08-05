f49e635b97d92c502b3f7842494c6f4d90bd039b
Damian <damgiankakis@gmail.com>
2017-08-05 11:27:51 +0300

    Fixes bugs on functions::getCurretUrl

--- includes/scripts/functions.php
+++ includes/scripts/functions.php
@@ -103,8 +103,10 @@ class Functions {
             $pageURL .= ":".$_SERVER["SERVER_PORT"];
         }
 		
-		if($_SERVER['SERVER_NAME'] === 'localhost') {
+		if($_SERVER['SERVER_NAME'] === 'localhost' || (strpos($_SERVER['SERVER_NAME'], '192.168.1') > -1)) {
 			$pageURL .= '/podokomia/';
+		} else {
+			$pageURL .= '/';
 		}
 	
 		return $pageURL;
