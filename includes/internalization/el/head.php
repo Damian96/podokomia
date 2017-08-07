603d65c09507f4f5b551c0b78d4ea13ebbc43460
Damian <damgiankakis@gmail.com>
2017-08-07 20:29:58 +0300

    Continues fixing of bugs

--- includes/internalization/el/head.php
+++ includes/internalization/el/head.php
@@ -5,6 +5,8 @@
     <script>!function(a,b,c,d,e,f,g){a.GoogleAnalyticsObject=e,a[e]=a[e]||function(){(a[e].q=a[e].q||[]).push(arguments)},a[e].l=1*new Date,f=b.createElement(c),g=b.getElementsByTagName(c)[0],f.async=1,f.src=d,g.parentNode.insertBefore(f,g)}(window,document,"script","https://www.google-analytics.com/analytics.js","ga"),ga("create","UA-74692550-9","auto"),ga("send","pageview");</script>
 	<?php } ?>
     <meta name="description" content="Βασίλης Ζαφειράκης κτηνίατρος  παραγωγικών ζώων, νύχια / εξονυχισμό αγελάδων, Θεσσαλονίκη. κτηνιατρος μεγαλων και παραγωγικων ζωων θεσσαλονικη"/>
+    <link rel="stylesheet" href="css/layout.css" />
+    <link rel="stylesheet" href="css/podokomia-icons.css" />
     <meta charset="utf-8"/>
 	<meta http-equiv="cache-control" content="max-age=1296000" />
     <meta name="robots" content="index,follow,noodp"/>
@@ -18,13 +20,11 @@
     <meta name="ICBM" content="40.62, 22.96" />
 	<meta name="google-site-verification" content="JVBFzUZhTGBX1Xfb4mIoYLrYg9cJskVOMv41S97pF8M" />
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
-    <link rel="stylesheet" href="css/layout.css" />
 	<link rel="stylesheet" href="css/<?=constant('ACTION')?>.css" />
     <link rel="stylesheet" media="(min-width : 320px)" href="css/min-320.css" />
     <link rel="stylesheet" media="(min-width : 480px)" href="css/min-480.css" />
     <link rel="stylesheet" media="(min-width : 768px)" href="css/min-768.css" />
     <link rel="stylesheet" media="(min-width : 1440px)" href="css/min-1440.css" />
-	<link rel="stylesheet" href="css/podokomia-icons.css" />
 	<link rel="stylesheet" href="css/slideshow.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
