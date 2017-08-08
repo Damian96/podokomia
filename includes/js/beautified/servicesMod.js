18a0dc9bd19ce50468f04f4e8c634b955c5e48d0
Damian <damgiankakis@gmail.com>
2017-08-08 20:57:26 +0300

    Adds on out click modal hiding

--- includes/js/beautified/servicesMod.js
+++ includes/js/beautified/servicesMod.js
@@ -1,34 +1,48 @@
-var modal;
+var modal, modalContent, modalImg, captionText;
 
 $(window).on('load', function() {
     modal = $('#servImgModal');
-
-    var images = $('.service-img');
-    var modalImg = $("#modImg01");
-    var captionText = $("#modCaption");
+    modalContent = modal.find('.modal-content');
+    modalImg = $("#modImg01");
+    captionText = $("#modCaption");
+    
+    var images = $('.service-img'),
+        span = modal.find('.close');
 
     images.click(function() {
         modal.show();
         $(window).on('keydown', escapeModal);
+        modalContent.attr('href', $(this).attr('src'))
         modalImg.attr('src', $(this).attr('src'));
         captionText.html($(this).attr('alt'));
+        setTimeout(function() {
+            $('body').on('click', destroyModal);
+        }, 0);
     });
-
-    var span = modal.find('.close');
 
     span.click(function() {
-        modal.hide();
-        $(window).off('keydown');
-    });
-    
-    modalImg.click(function() {
-       window.location.replace($(this).attr('src')); 
+        hideModal();
     });
 });
 
 function escapeModal(event) {
     if(event.keyCode == 27) {
-        modal.hide();
+        hideModal();
     }
     return true;
+}
+
+function destroyModal() {
+    if(($(this).closest('.modal-content').length <= 0) || $(this).closest('#modCaption').length <= 0) {
+        hideModal();
+    }
+}
+
+function hideModal() {
+    modal.hide();
+    $(window).off('keydown', escapeModal);
+    $('body').off('click', destroyModal);
+    modalContent.attr('href', '');
+    modalImg.attr('src', '');
+    captionText.html('');
 }
--- includes/serv-modal.php
+++ includes/serv-modal.php
@@ -1,5 +1,7 @@
 <div id="servImgModal" class="modal">
-  <span class="close" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
-  <img class="modal-content" id="modImg01">
-  <div id="modCaption"></div>
+    <span class="close" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+    <a class="modal-content" target="_blank">
+        <img id="modImg01">    
+    </a>
+    <div id="modCaption"></div>
 </div>
