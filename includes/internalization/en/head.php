44d30414ec848efc7dd789deaa2a4af35b6381a5
Damian <damgiankakis@gmail.com>
2017-10-21 14:50:36 +0300

    Fixes bug on en/services head.php

--- includes/internalization/en/head.php
+++ includes/internalization/en/head.php
@@ -1,6 +1,6 @@
 <head>
     <link rel="icon" href="<?= constant('LINK') ?>/favicon.ico" type="image/x-icon" />
-    <title><?=$title?></title>
+    <title><?=constant('TITLE')?></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="<?= constant('LINK') ?>/css/layout.min.css" />
