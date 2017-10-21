b6ced7d329a2c1a20738f4fce8615f61be28272c
Damian <damgiankakis@gmail.com>
2017-10-21 17:06:18 +0300

    Fixing nav on serv-modal

--- js/beautified/servicesMod.js
+++ js/beautified/servicesMod.js
@@ -1,18 +1,18 @@
-var modal, modalContent, modalImg, captionText;
+var originalImages, modal, modalContent, modalImg, captionText, modalImgIndex;
 
 $(window).on('load', function() {
     modal = $('#servImgModal');
     modalContent = modal.find('.modal-content');
     modalImg = $("#modImg01");
-    captionText = $("#modCaption");
-    
-    var images = $('.service-img'),
-        span = modal.find('.close');
+    captionText = $("#modCaption")
+    originalImages = $('.service-img');
+    var span = modal.find('.close');
 
-    images.click(function() {
+    originalImages.click(function() {
         modal.show();
+        modalImgIndex = getIndexOfClickedImg($(this).attr('src'));
         $(window).on('keydown', escapeModal);
-        modalContent.attr('href', $(this).attr('src'))
+        modalContent.attr('href', $(this).attr('src'));
         modalImg.attr('src', $(this).attr('src'));
         captionText.html($(this).attr('alt'));
         setTimeout(function() {
@@ -23,6 +23,8 @@ $(window).on('load', function() {
     span.click(function() {
         hideModal();
     });
+
+    modal.find('.glyphicon').click(handleControlClick);
 });
 
 function escapeModal(event) {
@@ -34,7 +36,9 @@ function escapeModal(event) {
 
 function destroyModal(event) {
     if((event.target.id !== 'modImg01') && (event.target.id !== 'modCaption')) {
-        hideModal();
+        if((event.target.id !== 'modal-controls') && !event.target.classList.contains('glyphicon')) {
+            hideModal();
+        }
     }
 }
 
@@ -45,4 +49,41 @@ function hideModal() {
     modalContent.attr('href', '');
     modalImg.attr('src', '');
     captionText.html('');
+}
+
+function handleControlClick(event) {
+    var newIndex, newImage,
+        length = originalImages.length - 1;
+    if(event.target.classList.contains('glyphicon-arrow-left')) {
+        if((modalImgIndex - 1) >= 0) {
+            newIndex = modalImgIndex - 1;
+        } else {
+            newIndex = length;
+        }
+    } else if(event.target.classList.contains('glyphicon-arrow-right')) {
+        if((modalImgIndex + 1) <= length) {
+            newIndex = modalImgIndex + 1;
+        } else {
+            newIndex = 0;
+        }
+    } else {
+        return;
+    }
+    newImage = originalImages.eq(newIndex);
+    modalImg.attr('src', newImage.attr('src'));
+    captionText.html(newImage.attr('alt'));
+    // modalContent.attr('href', $(this).attr('src'));
+    modalImgIndex = newIndex;
+    return true;
+}
+
+function getIndexOfClickedImg(src) {
+    var number;
+    originalImages.each(function() {
+        if($(this).attr('src') === src) {
+            number = $(this).index();
+            return false;
+        }
+    });
+    return number;
 }
