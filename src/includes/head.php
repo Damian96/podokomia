8ff0b1809b8257f06595505b229dae1a22c90b8c
Damian96 <damgiankakis@gmail.com>
2018-02-05 11:58:07 +0200

    Adds new analytics code, removes onload and fixes bugs

--- src/includes/head.php
+++ src/includes/head.php
@@ -46,7 +46,15 @@
 <!-- </Responsive CSS> -->
 <!-- <Google Analytics Script> -->
 <?php if ($functions->isLocal()): ?>
-<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','UA-74692550-9');</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-74692550-9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-74692550-9');
+</script>
 <?php endif ?>
 <!-- </Google Analytics Script> -->
 <!-- <Javascript> -->
