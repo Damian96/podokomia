4dfcc87c62f5156222788f187b5a80ca96f0baa0
Damian96 <damiang23ftr@gmail.com>
2018-07-10 15:04:08 +0300

    Changes internalisation folder name, and minor bugs

---
+++ src/includes/internalisation/en/services.php
@@ -0,0 +1,22 @@
+<div class="section" id="avail-services">
+	<h1>Services</h1>
+	<ul>
+		<li>Fertility Management with ultrasound scanner</li>
+		<li>Impaired Cow Care</li>
+		<li>Dairy Herd Management</li>
+		<li>24 Hour Emergency Service</li>
+		<li>Disease Control - Herd Vaccination Protocols</li>
+		<li>Surgical Services (on farm)</li>
+		<li>Preventative and Curative Cattle Hoof Trimming</li>
+		<li>Foot Health Programs</li>
+		<li>Locomotion Scoring</li>
+		<li>Metabolic Disease Control</li>
+		<li>Nutrition Consultation</li>
+	</ul>
+</div>
+<div id="serv-images">
+    <img alt="service-1" src="<?php echo BASEURL . '/images/services-1.jpg' ?>" class="modal-image" alt="Dairy Herd Management"/>
+    <img alt="service-2" src="<?php echo BASEURL . '/images/services-2.jpg' ?>" class="modal-image" alt="Locomotion Scoring"/>
+    <img alt="service-3" src="<?php echo BASEURL . '/images/services-3.jpg' ?>" class="modal-image" alt="services-3"/>
+    <img alt="service-4" src="<?php echo BASEURL . '/images/services-4.jpg' ?>" class="modal-image" alt="Fertility Management with ultrasound scanner"/>
+</div>
