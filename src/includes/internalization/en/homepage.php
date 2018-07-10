adda182b9f36f9d9579aac7f90811e11bd5411b9
Damian96 <damiang23ftr@gmail.com>
2018-07-10 15:32:50 +0300

    Merges with dev_og, reverts to original nodejs site

--- src/includes/internalization/en/homepage.php
+++
@@ -1,6 +0,0 @@
-				<p id="home-desc">
-					Vasilis Zafeirakis is a dairy vet who graduated from the Aristotle University of Thessaloniki in 1996. He has attended the Hoof Τrimming Seminar PTC Oenkerk in Netherlands (2001). He is particularly interested in bovine lameness, hoof trimming, surgery, internal medicine and nutrition.
-				</p>
-				<p>
-					<img id="home-img" alt="main-page-cow-image" src="<?php echo BASEURL ?>/images/home.jpg"/>
-				</p>
